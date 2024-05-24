eef533f48115ecc7c2238f9fda9caea0f37690cc
WQYeo <yeo_dev@protonmail.com>
2024-05-24 19:27:06 +0800

    seperate docker file for production usage

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!');
+app.get('/api/ping', (req: Request, res: Response) => {
+	res.send('Pong!');
 });
 
 app.listen(port, () => {
