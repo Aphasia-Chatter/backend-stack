c57f9a2aa558ff435c8ee11b03d7f3c69fdb1725
wqyeo <yeo_dev@protonmail.com>
2024-05-30 04:15:19 +0800

    Made app auto-create default staff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
 
+import * as argon2 from "argon2";
+import * as schema from './schema';
+import { db } from './db';
+
 const app = express();
 const port = 3000;
 
@@ -9,4 +13,30 @@ app.get('/api/ping', (req: Request, res: Response) => {
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
+
+	db.select().from(schema.staff).then(async (result) => {
+		if (result.length >= 1) {
+			return;
+		}
+
+		const defaultStaffUsername = process.env.DEFAULT_STAFF_USERNAME || 'staff'
+		const defaultStaffPassword = process.env.DEFAULT_STAFF_PASSWORD || 'adminadmin'
+
+		const hashPepper = process.env.HASHING_PEPPER
+
+		try {
+			const hash = await argon2.hash(defaultStaffPassword + hashPepper);
+			await db.insert(schema.staff).values({
+				username: defaultStaffUsername,
+				hashedPassword: hash
+			});
+
+		} catch (err) {
+			await db.insert(schema.log).values({
+				message: `Failed to create default staff user :: ${err}`,
+				severity: "CRITICAL"
+			})
+		}
+	})
+
 });
