305325cfc1978a78cdfb7174bc7ea852eda63508
Xin Long <2200563@sit.singaporetech.edu.sg>
2024-06-12 02:52:53 +0800

    Added staff register route

--- src/api/routes/staffRoutes.ts
+++ src/api/routes/staffRoutes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import login, { LoginType } from '../controllers/login';
+import register, { RegisterType } from '../controllers/register';
 import checkToken from '../controllers/checkToken';
 
 const router: Router = Router();
@@ -13,5 +14,10 @@ router.post('/login', (req: Request, res: Response) => {
 router.get('/validate-token', (req: Request, res: Response) => {
   checkToken(req, res)
 });
+
+// /api/staff/register
+router.post('/register', (req: Request, res: Response) => {
+  register(req, res, RegisterType.STAFF)
+})
 
 export default router;
