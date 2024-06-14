13b00534e10d0bdcffb96c68b397193aec9f9818
Farhan Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2024-06-14 16:33:03 +0800

    Removed unused code

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,6 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
 const app = express();
 
 /*
