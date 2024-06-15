6c31afb04e55e5f55a8a113f8826f96760949ec9
WQYeo <yeo_dev@protonmail.com>
2024-06-15 19:13:47 +0800

    database function to select word retrevial task by ID

---
+++ src/api/repositories/selectWordRetrevialTaskByID.ts
@@ -0,0 +1,12 @@
+import { db } from "src/db"
+import { task, taskEditor, wordRetrievalTask, wordRetrievalTaskHint } from "src/schema"
+import { eq } from "drizzle-orm"
+
+export default async function selectWordRetrevialTaskByID(id: string) {
+    return await db.select()
+    .from(task)
+    .where(eq(task.id, id))
+    .innerJoin(wordRetrievalTask, eq(task.id, wordRetrievalTask.taskID))
+    .innerJoin(taskEditor, eq(taskEditor.taskID, task.id))
+    .innerJoin(wordRetrievalTaskHint, eq(wordRetrievalTaskHint.taskID, wordRetrievalTask.taskID))
+}
