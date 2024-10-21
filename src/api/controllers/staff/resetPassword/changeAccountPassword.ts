a575eeb187aa34fd078abebe946fdec359b01d3f
KeeHanXiang <khx07602@gmail.com>
2024-10-21 15:39:35 +0800

    Update changeAccountPassword.ts
    - Applied staff password verification
    - Fixed Modal status display

--- src/api/controllers/staff/resetPassword/changeAccountPassword.ts
+++ src/api/controllers/staff/resetPassword/changeAccountPassword.ts
@@ -6,8 +6,8 @@ import updatePatientPassword from '../../../repositories/updatePatientPassword';
 import selectPatientByUsername from '../../../repositories/selectPatientByUsernameAndToken';
 import validateHash from '../../../utils/validateHash';
 import deleteAllPatientSessionToken from '../../../repositories/deleteAllPatientSessionToken';
-import deleteAllStaffSessionToken from '../../../repositories/deleteAllStaffSessionToken';
 import getStaffPassword from '../../../repositories/getStaffPassword';
+import selectStaffByUsername from 'src/api/repositories/selectStaffByUsername';
 
 export enum ChangeAccountPasswordType {
     STAFF,
@@ -33,76 +33,72 @@ export default async function changeAccountPassword(req: Request, res: Response,
 
     if (!jsonReq.staffUsername) {
         return res.status(400).json({
-            'status': 'MISSING_STAFF_USERNAME',
+            'status': 'MISSING STAFF USERNAME',
             'message': 'staff username is missing in the request body field.'
         });
     }
 
     if (!jsonReq.sessionToken) {
         return res.status(400).json({
-            'status': 'MISSING_SESSION',
+            'status': 'MISSING SESSION',
             'message': 'session is missing in the request body field.'
         });
     }
 
     if (!jsonReq.patientUsername) {
         return res.status(400).json({
-            'status': 'MISSING_CURRENT_PASSWORD',
+            'status': 'MISSING CURRENT PASSWORD',
             'message': 'patient username is missing in the request body field.'
         });
     }
 
     if (!jsonReq.newPatientPassword) {
         return res.status(400).json({
-            'status': 'MISSING_NEW_PASSWORD',
+            'status': 'MISSING NEW PASSWORD',
             'message': 'new password is missing in the request body field.'
         });
     }
 
     if (!jsonReq.staffPassword) {
         return res.status(400).json({
-            'status': 'MISSING_CONFIRM_NEW_PASSWORD',
+            'status': 'MISSING CONFIRM NEW PASSWORD',
             'message': 'staff password is missing in the request body field.'
         });
     }
 
-    if (jsonReq.newPatientPassword != jsonReq.patientConfirmPassword) {
-        return res.status(400).json({
-            'status': 'MISMATCH_NEW_PASSWORD',
-            'message': 'New Password and confirm new password in the request body fields do not match.',
-            'data': {}
+    try {
+        const result = await selectStaffByUsername(jsonReq.staffUsername)
+
+        const relatedUser = result[0]
+        
+        if (jsonReq.newPatientPassword != jsonReq.patientConfirmPassword) {
+            return res.status(400).json({
+                'status': 'MISMATCHED PASSWORD',
+                'message': 'New Password and confirm new password in the request body fields do not match.',
+                'data': {}
+            });
+        }    
+
+        // added await because validateHash is a background task
+        if (!(await validateHash(jsonReq.staffPassword, relatedUser.hashedPassword))) {
+            return res.status(400).json({
+                'status': 'WRONG STAFF PASSWORD',
+                'message': 'Password mismatch!',
+                'data': {}
+            });
+        }
+
+        // Continue with patient account password change process
+        await changePatientAccountPassword(jsonReq as ChangeAccountPasswordRequest, res);
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({
+            'status': 'SERVER_ERROR',
+            'message': 'An error occurred while processing the request.',
         });
     }
 
-    // try {
-        
-    //     // Retrieve the hashed staff password from the database
-    //     const staffHashedPassword = await getStaffPassword(jsonReq.staffUsername);
-
-    //     // Verify the entered staff password matches the stored hashed password
-    //     const isStaffPasswordValid = await argon2.verify(staffHashedPassword, jsonReq.staffPassword);
-
-    //     if (!isStaffPasswordValid) {
-    //         return res.status(400).json({
-    //             'status': 'INVALID_STAFF_PASSWORD',
-    //             'message': 'Staff password is incorrect.'
-    //         });
-    //     }
-
-    //     // Continue with patient account password change process
-    //     await changePatientAccountPassword(jsonReq as ChangeAccountPasswordRequest, res);
-    // } catch (error) {
-    //     console.error('Error:', error);
-    //     return res.status(500).json({
-    //         'status': 'SERVER_ERROR',
-    //         'message': 'An error occurred while processing the request.',
-    //     });
-    // }
-
-    await changePatientAccountPassword(jsonReq as ChangeAccountPasswordRequest, res);
-
 }
-
 
 // Patient Change Account Password Function
 async function changePatientAccountPassword(jsonReq: ChangeAccountPasswordRequest, res: Response) {
@@ -134,18 +130,18 @@ async function changePatientAccountPassword(jsonReq: ChangeAccountPasswordReques
             await deleteAllPatientSessionToken(relatedPatient.id)
 
             return res.status(200).json({
-                'status': 'CHANGE_ACCOUNT_PASSWORD_SUCCESS',
-                'message': 'Patient account password update is successful. You will be logged out now.',
+                'status': 'SUCCESS',
+                'message': 'Patient account password update is successful.',
             });
         } catch (err) {
             return res.status(400).json({
-                'status': 'HASHING_ERROR',
+                'status': 'HASHING ERROR',
                 'message': 'Failed to update patient account new password due to hashing issues.'
             });
         }
     } catch (err) {
         return res.status(500).json({
-            'status': 'SERVER_ERROR',
+            'status': 'SERVER ERROR',
             'message': 'Server encountered an error! Contact admin if persists!',
         });
     }
